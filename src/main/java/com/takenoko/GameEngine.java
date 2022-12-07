fbaac90420520f71d4f50c09961b7061115acd17
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2022-12-07 22:03:28 +0100

    feat: create game engine first implementation
    It creates a board with a single tile in it and display some messages about what is happening in the game.
    Refs: #58
    
    Signed-off-by: neoteristis <anjou.raphael@gmail.com>
    Signed-off-by: Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>

---
+++ src/main/java/com/takenoko/GameEngine.java
@@ -0,0 +1,24 @@
+package com.takenoko;
+
+import com.takenoko.ui.ConsoleUserInterface;
+
+public class GameEngine {
+    private final Board board;
+    private final ConsoleUserInterface consoleUserInterface;
+
+    public GameEngine() {
+        board = new Board();
+        consoleUserInterface = new ConsoleUserInterface();
+    }
+
+    public void startGame() {
+        consoleUserInterface.displayMessage("Welcome to Takenoko!");
+
+        Tile tile = new Tile();
+        board.placeTile(tile);
+
+        consoleUserInterface.displayMessage(
+                "The board is now set up with a single tile. You can start the game !");
+        consoleUserInterface.displayMessage("The game is finished. Thanks for playing !");
+    }
+}
