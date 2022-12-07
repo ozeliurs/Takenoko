87cbfc3a1bcae8cea9b3bfcf2f2e6e8bdbeb3b04
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2022-12-08 00:05:38 +0100

    docs: add javadoc to the needed methods
    Refs: #59

--- src/main/java/com/takenoko/Bot.java
+++ src/main/java/com/takenoko/Bot.java
@@ -6,11 +6,18 @@ public class Bot implements Playable {
     private final Board board;
     private final ConsoleUserInterface consoleUserInterface;
 
+    /**
+     * Constructor for the Bot class. Instantiate a user interface for the bot.
+     * @param board the board of the game the bot is currently on
+     */
     public Bot(Board board) {
         this.board = board;
         this.consoleUserInterface = new ConsoleUserInterface();
     }
 
+    /**
+     * This method place on the board the only available tile.
+     */
     @Override
     public void placeTile() {
         try {
