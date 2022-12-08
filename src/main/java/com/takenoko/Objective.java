4f034e237e2bccf00e6558fdea529e66285f3351
Sayf Halmi <sayf-eddine.halmi@etu.unice.fr>
2022-12-08 20:23:16 +0100

    create Objective class with its methods and accessors

---
+++ src/main/java/com/takenoko/Objective.java
@@ -0,0 +1,36 @@
+package com.takenoko;
+
+public class Objective {
+    private int counterGoal;
+    private String type;
+    private boolean status;
+    private int counter;
+
+    public Objective(int counterGoal,String type){
+        this.counterGoal=counterGoal;
+        this.type=type;
+        status=false;
+        counter=0;
+    }
+
+    public Objective(){
+        this(0,"to be determined");
+    }
+
+    public void setAction(int counterGoal,String type){
+        this.type=type;
+        this.counterGoal=counterGoal;
+    }
+
+    public void incrementCounter(){
+        counter++;
+    }
+
+    public boolean isAchieved(){
+        return status=(counter==counterGoal);
+    }
+
+    public String toString(){
+        return "You must place "+counterGoal+" "+type;
+    }
+}
