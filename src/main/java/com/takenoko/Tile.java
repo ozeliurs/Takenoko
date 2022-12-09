a7f4ee5abc621210433c2870496954c2b8029759
Ozeliurs <ozeliurs@gmail.com>
2022-12-09 08:41:13 +0100

    feat(tile): add isPond property to tile

--- src/main/java/com/takenoko/Tile.java
+++ src/main/java/com/takenoko/Tile.java
@@ -1,3 +1,14 @@
 package com.takenoko;
 
-public class Tile {}
+public class Tile {
+    private boolean isPond = false;
+
+    public boolean getPond() {
+        return isPond;
+    }
+
+    public void setPond(boolean isPond) {
+        this.isPond = isPond;
+    }
+
+}
