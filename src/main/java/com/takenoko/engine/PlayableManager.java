008550a7c3f5bc642d19c8f6bf0a4efae984c117
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-12-09 22:23:37 +0100

    refactor: remove useless method closes #78

--- src/main/java/com/takenoko/engine/PlayableManager.java
+++ src/main/java/com/takenoko/engine/PlayableManager.java
@@ -9,16 +9,12 @@ public abstract class PlayableManager {
     private static final int DEFAULT_NUMBER_OF_ACTIONS = 2;
     private final int numberOfActions;
     private final Objective placeTileObjective;
-    private ConsoleUserInterface consoleUserInterface;
+    private final ConsoleUserInterface consoleUserInterface;
 
     protected PlayableManager() {
         numberOfActions = DEFAULT_NUMBER_OF_ACTIONS;
         placeTileObjective = new PlaceTileObjective(2);
         consoleUserInterface = new ConsoleUserInterface();
-    }
-
-    public void setConsoleUserInterface(ConsoleUserInterface consoleUserInterface) {
-        this.consoleUserInterface = consoleUserInterface;
     }
 
     public String getObjectiveDescription() {
