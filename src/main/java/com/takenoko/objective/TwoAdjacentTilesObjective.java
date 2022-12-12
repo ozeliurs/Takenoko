8b6b84e0b889e0b96c77a5f8705cd8cc35ebba36
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2022-12-12 14:24:16 +0100

    fix(two-adjacent-tiles-objective): pond was taken into account
    Refs: #49

--- src/main/java/com/takenoko/objective/TwoAdjacentTilesObjective.java
+++ src/main/java/com/takenoko/objective/TwoAdjacentTilesObjective.java
@@ -3,12 +3,23 @@ package com.takenoko.objective;
 import com.takenoko.Board;
 import com.takenoko.shape.Adjacent;
 import com.takenoko.shape.Shape;
-import java.util.List;
+import com.takenoko.tile.Tile;
+import com.takenoko.vector.Vector;
 
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The objective is to have two tiles next to each other.
+ * Of course, the pond is not taken into account as the bot would win when placing its first tile.
+ */
 public class TwoAdjacentTilesObjective implements Objective {
     private static final ObjectiveTypes type = ObjectiveTypes.TWO_ADJACENT_TILES;
     private ObjectiveState state;
 
+    /**
+     * Constructor for the class
+     */
     public TwoAdjacentTilesObjective() {
         state = ObjectiveState.NOT_ACHIEVED;
     }
@@ -32,7 +43,8 @@ public class TwoAdjacentTilesObjective implements Objective {
 
     public void verify(Board board) {
         Shape adjacentShape = new Adjacent();
-        List<Shape> matchingShapes = adjacentShape.match(board.getTiles());
+        Map<Vector, Tile> boardTiles = board.getTilesWithoutPond();
+        List<Shape> matchingShapes = adjacentShape.match(boardTiles);
         if (!matchingShapes.isEmpty()) {
             state = ObjectiveState.ACHIEVED;
         }
