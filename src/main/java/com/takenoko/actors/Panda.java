38ffaafd260c7e1c264de8f933d6c9cd3241e052
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2022-12-13 11:22:47 +0100

    feat(panda): add positionMessage
    Refs #116

--- src/main/java/com/takenoko/actors/Panda.java
+++ src/main/java/com/takenoko/actors/Panda.java
@@ -8,11 +8,25 @@ import com.takenoko.vector.Vector;
 public class Panda {
     Vector position;
 
+    /**
+     * Constructor for the Panda class. Instantiate the panda at the origin.
+     */
     public Panda() {
         this.position = new Vector(0, 0, 0);
     }
 
+    /**
+     *
+     * @return the position of the panda
+     */
     public Vector getPosition() {
         return position;
+    }
+
+    /**
+     * @return a string explaining where the panda is on the board
+     */
+    public String positionMessage() {
+        return "The panda is at " + position;
     }
 }
