d5847e5c3ad02cb4e76795636ab00c9af14833e6
Ozeliurs <ozeliurs@gmail.com>
2022-12-14 16:23:00 +0100

    feat(movedpandaobjective): created class wth its methods
    Ref #124

---
+++ src/main/java/com/takenoko/objective/MovedPandaObjective.java
@@ -0,0 +1,38 @@
+package com.takenoko.objective;
+
+import com.takenoko.Board;
+import com.takenoko.actors.Panda;
+import com.takenoko.vector.PositionVector;
+
+public class MovedPandaObjective implements Objective {
+    private ObjectiveState state;
+
+    /** Constructor for the class 🤯*/
+    public MovedPandaObjective() {
+        state = ObjectiveState.NOT_ACHIEVED;
+    }
+
+    /**
+     * This will check if the panda has moved from
+     * its initial position (0, 0, 0).
+     *
+     * @param board the current board of the game
+     */
+    @Override
+    public void verify(Board board) {
+        Panda panda = board.getPanda();
+        if (!panda.getPosition().equals(new PositionVector(0, 0, 0))) {
+            state = ObjectiveState.ACHIEVED;
+        }
+    }
+
+    /**
+     * Allows checking if the objective has been achieved
+     *
+     * @return whether the objective has been achieved or not
+     */
+    @Override
+    public boolean isAchieved() {
+        return state == ObjectiveState.ACHIEVED;
+    }
+}
