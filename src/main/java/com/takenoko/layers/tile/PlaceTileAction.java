2578d0d8f1373fc94b2f9a49685bcf4fba4ddef6
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-12-15 23:16:59 +0100

    docs: add JavaDocs to Action, MovePandaAction, PlaceTileAction
    Ref: #123

--- src/main/java/com/takenoko/layers/tile/PlaceTileAction.java
+++ src/main/java/com/takenoko/layers/tile/PlaceTileAction.java
@@ -6,15 +6,28 @@ import com.takenoko.player.Action;
 import com.takenoko.tile.Tile;
 import com.takenoko.vector.PositionVector;
 
+/** This class represents the action of placing a tile on the board. */
 public class PlaceTileAction implements Action {
     private final Tile tile;
     private final PositionVector positionVector;
 
+    /**
+     * Constructor for the PlaceTileAction class.
+     *
+     * @param tile the tile to place
+     * @param positionVector the position vector where to place the tile
+     */
     public PlaceTileAction(Tile tile, PositionVector positionVector) {
         this.tile = tile;
         this.positionVector = positionVector;
     }
 
+    /**
+     * Place the tile on the board and display a message.
+     *
+     * @param board the board
+     * @param botManager the bot manager
+     */
     @Override
     public void execute(Board board, BotManager botManager) {
         board.getLayerManager().getTileLayer().placeTile(tile, positionVector);
--- src/main/java/com/takenoko/player/Action.java
+++ src/main/java/com/takenoko/player/Action.java
@@ -5,5 +5,11 @@ import com.takenoko.engine.BotManager;
 
 /** Action interface. An action is a command that can be executed by the Bot Manager. */
 public interface Action {
+    /**
+     * Executes the action on the board and the bot manager.
+     *
+     * @param board the board
+     * @param botManager the bot manager
+     */
     void execute(Board board, BotManager botManager);
 }
