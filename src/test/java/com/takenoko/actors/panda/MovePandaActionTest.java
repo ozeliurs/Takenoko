67825854afa17e4bca4295645aa7a2a2259b26a2
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-12-16 22:41:26 +0100

    fix: panda does not check if there is bamboo at position before eating
    Ref: #147

--- src/test/java/com/takenoko/actors/panda/MovePandaActionTest.java
+++ src/test/java/com/takenoko/actors/panda/MovePandaActionTest.java
@@ -7,6 +7,7 @@ import com.takenoko.engine.Board;
 import com.takenoko.engine.BotManager;
 import com.takenoko.layers.LayerManager;
 import com.takenoko.layers.bamboo.BambooLayer;
+import com.takenoko.layers.bamboo.BambooStack;
 import com.takenoko.player.Playable;
 import com.takenoko.vector.PositionVector;
 import org.junit.jupiter.api.BeforeEach;
@@ -21,6 +22,7 @@ class MovePandaActionTest {
     private Board board;
 
     private Panda panda;
+    private BambooLayer bambooLayer;
 
     @BeforeEach
     void setUp() {
@@ -28,11 +30,11 @@ class MovePandaActionTest {
         botManager = new BotManager(mock(Playable.class));
         panda = mock(Panda.class);
         ActorsManager actorsManager = mock(ActorsManager.class);
-        when(actorsManager.getPanda()).thenReturn(panda);
         board = mock(Board.class);
-        when(board.getActorsManager()).thenReturn(actorsManager);
         LayerManager layerManager = mock(LayerManager.class);
-        BambooLayer bambooLayer = mock(BambooLayer.class);
+        bambooLayer = mock(BambooLayer.class);
+        when(board.getActorsManager()).thenReturn(actorsManager);
+        when(actorsManager.getPanda()).thenReturn(panda);
         when(layerManager.getBambooLayer()).thenReturn(bambooLayer);
         when(board.getLayerManager()).thenReturn(layerManager);
     }
@@ -43,8 +45,22 @@ class MovePandaActionTest {
         @Test
         @DisplayName("should move the panda")
         void shouldMoveThePanda() {
+            when(panda.getPosition()).thenReturn(new PositionVector(0, 0, 0));
+            when(bambooLayer.getBambooAt(any())).thenReturn(new BambooStack(0));
             movePandaAction.execute(board, botManager);
             verify(panda).move(new PositionVector(-1, 0, 1));
+        }
+
+        @Test
+        @DisplayName("should move panda and eat bamboo")
+        void shouldMovePandaAndEatBamboo() {
+            when(panda.getPosition())
+                    .thenReturn(new PositionVector(0, 0, 0).add(new PositionVector(-1, 0, 1)));
+            when(bambooLayer.getBambooAt(any())).thenReturn(new BambooStack(1));
+            movePandaAction.execute(board, botManager);
+            verify(panda).move(new PositionVector(-1, 0, 1));
+            verify(bambooLayer).getBambooAt(new PositionVector(-1, 0, 1));
+            verify(bambooLayer).removeBamboo(new PositionVector(-1, 0, 1));
         }
     }
 }
