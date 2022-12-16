d65bebed704f780f4048038d12c32e205d1401fc
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-12-16 02:03:09 +0100

    docs: add docs to BambooLayer
    Ref: #127

--- src/main/java/com/takenoko/layers/bamboo/BambooLayer.java
+++ src/main/java/com/takenoko/layers/bamboo/BambooLayer.java
@@ -5,19 +5,36 @@ import com.takenoko.vector.PositionVector;
 import java.util.HashMap;
 import java.util.Map;
 
+/** BambooLayer class. The bamboo layer contains the number of bamboo on each tile. */
 public class BambooLayer {
     private final HashMap<PositionVector, Integer> bamboo;
     private final Board board;
 
+    /**
+     * Constructor for the BambooLayer class.
+     *
+     * @param board the board
+     */
     public BambooLayer(Board board) {
         bamboo = new HashMap<>();
         this.board = board;
     }
 
+    /**
+     * Add bamboo to a tile. By default, the number of bamboo is 1 if the tile is irrigated.
+     *
+     * @param positionVector the position of the tile
+     */
     void addBamboo(PositionVector positionVector) {
         bamboo.put(positionVector, bamboo.getOrDefault(positionVector, 1) + 1);
     }
 
+    /**
+     * Get the number of bamboo on a tile.
+     *
+     * @param positionVector the position of the tile
+     * @return the number of bamboo on the tile
+     */
     public int getBambooAt(PositionVector positionVector) {
         if (board.getLayerManager().getTileLayer().isTile(positionVector)) {
             return bamboo.getOrDefault(positionVector, 1);
@@ -26,7 +43,12 @@ public class BambooLayer {
         }
     }
 
+    /**
+     * Get a copy of the hashmap of bamboo.
+     *
+     * @return the bamboo
+     */
     public Map<PositionVector, Integer> getBamboo() {
-        return bamboo;
+        return new HashMap<>(bamboo);
     }
 }
