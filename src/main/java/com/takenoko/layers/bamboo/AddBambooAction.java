c428d550408b7907c3df74170b16da1f78bac1c0
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-12-16 14:40:19 +0100

    docs: add docs to AddBambooAction
    Ref: #127

--- src/main/java/com/takenoko/layers/bamboo/AddBambooAction.java
+++ src/main/java/com/takenoko/layers/bamboo/AddBambooAction.java
@@ -5,13 +5,27 @@ import com.takenoko.engine.BotManager;
 import com.takenoko.player.Action;
 import com.takenoko.vector.PositionVector;
 
+/**
+ * This class represents the action of adding a bamboo on the board.
+ */
 public class AddBambooAction implements Action {
     private final PositionVector positionVector;
 
+    /**
+     * Constructor for the AddBambooAction class.
+     *
+     * @param positionVector the position vector where to add a bamboo
+     */
     public AddBambooAction(PositionVector positionVector) {
         this.positionVector = positionVector;
     }
 
+    /**
+     * Add a bamboo on the board and display a message.
+     *
+     * @param board      the board to add a bamboo
+     * @param botManager the bot manager to display a message
+     */
     @Override
     public void execute(Board board, BotManager botManager) {
         board.getLayerManager().getBambooLayer().addBamboo(positionVector);
