db7b267f0f9b1c59a009b50d50b0d782a9ac141c
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-12-17 22:48:02 +0100

    docs: add docs to TilePlacingAndPandaMovingBot
    Ref: #160

--- src/main/java/com/takenoko/bot/TilePlacingAndPandaMovingBot.java
+++ src/main/java/com/takenoko/bot/TilePlacingAndPandaMovingBot.java
@@ -4,6 +4,10 @@ import com.takenoko.actors.panda.MovePandaAction;
 import com.takenoko.engine.Board;
 import com.takenoko.layers.tile.PlaceTileAction;
 
+/**
+ * This class is a bot that will place a tile if it can't move the panda, and move the panda when it
+ * can.
+ */
 public class TilePlacingAndPandaMovingBot implements Bot {
     @Override
     public Action chooseAction(Board board) {
