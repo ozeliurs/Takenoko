2ed0638181b173771b4fdb2272fd91774e6789f8
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2022-12-18 14:35:16 +0100

    feat: implement CollectBambooAction
    Ref: #130

---
+++ src/test/java/com/takenoko/inventory/CollectBambooActionTest.java
@@ -0,0 +1,26 @@
+package com.takenoko.inventory;
+
+import static org.mockito.Mockito.*;
+
+import com.takenoko.engine.BotManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class CollectBambooActionTest {
+
+    @Nested
+    @DisplayName("Method execute()")
+    class TestExecute {
+        @Test
+        @DisplayName("should add bamboo to the inventory")
+        void shouldAddBambooToTheInventory() {
+            BotManager botManager = mock(BotManager.class);
+            when(botManager.getInventory()).thenReturn(mock(Inventory.class));
+            when(botManager.getInventory().getBambooStack())
+                    .thenReturn(mock(InventoryBambooStack.class));
+            (new CollectBambooAction()).execute(null, botManager);
+            verify(botManager.getInventory().getBambooStack()).addBamboo();
+        }
+    }
+}
