6222db70ea629de0885f4ba151f4a6d0dc27616d
Ozeliurs <ozeliurs@gmail.com>
2023-01-06 13:06:18 +0100

    feat: add gardener moving bot
    Ref. #216

---
+++ src/main/java/com/takenoko/bot/TilePlacingAndGardenerMovingBot.java
@@ -0,0 +1,25 @@
+package com.takenoko.bot;
+
+import com.takenoko.actors.gardener.MoveGardenerAction;
+import com.takenoko.engine.Board;
+import com.takenoko.engine.BotState;
+import com.takenoko.layers.tile.PlaceTileAction;
+
+/**
+ * This class is a bot that will place a tile if it can't move the panda, and move the panda when it
+ * can.
+ */
+public class TilePlacingAndGardenerMovingBot implements Bot {
+    @Override
+    public Action chooseAction(Board board, BotState botState) {
+        // check if the gardener can move
+        if (board.getGardenerPossibleMoves().isEmpty()) {
+            // place a tile
+            return new PlaceTileAction(
+                    board.getAvailableTiles().get(0), board.getAvailableTilePositions().get(0));
+        } else {
+            // move the panda
+            return new MoveGardenerAction(board.getGardenerPossibleMoves().get(0));
+        }
+    }
+}
