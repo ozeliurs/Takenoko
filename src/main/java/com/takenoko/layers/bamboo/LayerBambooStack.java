5249dfe2743dc1ba187dde9f225334a672f25e20
Ozeliurs <ozeliurs@gmail.com>
2023-01-06 15:37:07 +0100

    fix: added type checking for growing bamboo
    Ref. #218

--- src/main/java/com/takenoko/layers/bamboo/LayerBambooStack.java
+++ src/main/java/com/takenoko/layers/bamboo/LayerBambooStack.java
@@ -1,10 +1,18 @@
 package com.takenoko.layers.bamboo;
 
+import java.util.Objects;
+
 public class LayerBambooStack extends BambooStack {
     private static final int MAX_BAMBOO = 4;
+    private boolean isImmutable = false;
 
     public LayerBambooStack(int startingBamboo) {
         super(startingBamboo);
+    }
+
+    public LayerBambooStack(int startingBamboo, boolean isImmutable) {
+        super(startingBamboo);
+        this.isImmutable = isImmutable;
     }
 
     @Override
@@ -23,8 +31,36 @@ public class LayerBambooStack extends BambooStack {
         bambooCount--;
     }
 
+    /*
+     * @return if the bamboo can grow
+     */
+    public boolean isGrowable() {
+        return bambooCount < MAX_BAMBOO && !isImmutable;
+    }
+
+    /*
+     * @return if the bamboo can be eaten
+     */
+    public boolean isEatable() {
+        return bambooCount > 0 && !isImmutable;
+    }
+
     @Override
     public LayerBambooStack copy() {
         return new LayerBambooStack(this.getBambooCount());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        LayerBambooStack that = (LayerBambooStack) o;
+        return isImmutable == that.isImmutable;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), isImmutable);
     }
 }
