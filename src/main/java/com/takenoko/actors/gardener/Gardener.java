14cd71d9e46b18131362bd8fda96aec789f5a3c8
Ozeliurs <ozeliurs@gmail.com>
2023-01-06 15:06:05 +0100

    fix: formatting
    Ref. #218

--- src/main/java/com/takenoko/actors/gardener/Gardener.java
+++ src/main/java/com/takenoko/actors/gardener/Gardener.java
@@ -31,7 +31,8 @@ public class Gardener extends com.takenoko.actors.Actor {
 
     public LayerBambooStack afterMove(Board board) {
         // check if the gardener can grow bamboo (not pond)
-        if (board.getBambooAt(this.getPositionVector()).getBambooCount() < 3 && board.getTileAt(this.getPositionVector()).getType() != TileType.POND) {
+        if (board.getBambooAt(this.getPositionVector()).getBambooCount() < 3
+                && board.getTileAt(this.getPositionVector()).getType() != TileType.POND) {
             // grow bamboo
             board.growBamboo(this.getPositionVector());
             return new LayerBambooStack(1);
