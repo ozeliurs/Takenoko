cb8785a4de703f64acfcb102bfd9d4e34d08d407
Ozeliurs <ozeliurs@gmail.com>
2023-01-09 15:17:23 +0100

    fix: switch MovePandaActionTest and PandaTest
    Ref. #255

--- src/test/java/com/takenoko/actions/MovePandaActionTest.java
+++ src/test/java/com/takenoko/actions/MovePandaActionTest.java
@@ -1,8 +1,7 @@
-package com.takenoko.actors;
+package com.takenoko.actions;
 
 import static org.mockito.Mockito.*;
 
-import com.takenoko.actions.MovePandaAction;
 import com.takenoko.engine.Board;
 import com.takenoko.engine.BotManager;
 import com.takenoko.inventory.Inventory;
