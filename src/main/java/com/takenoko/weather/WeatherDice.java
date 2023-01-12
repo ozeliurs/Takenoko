b0431c200935eeacaee526a0562ebfb95b2804f6
Sayf <sayf-eddine.halmi@etu.unice.fr>
2023-01-12 16:42:46 +0100

    fix: remove switch from WeatherDice.rollWeather use Weather.values() instead
    Ref: #263

--- src/main/java/com/takenoko/weather/WeatherDice.java
+++ src/main/java/com/takenoko/weather/WeatherDice.java
@@ -25,14 +25,9 @@ public class WeatherDice extends Dice {
      */
     public Weather rollWeather() {
         int roll = super.roll();
-        return switch (roll) {
-            case 1 -> Weather.SUNNY;
-            case 2 -> Weather.RAINY;
-            case 3 -> Weather.CLOUDY;
-            case 4 -> Weather.WINDY;
-            case 5 -> Weather.STORM;
-            case 6 -> Weather.RANDOM;
-            default -> throw new IllegalStateException("Unexpected value: " + roll);
-        };
+        if((roll>=6)||(roll<0)){
+            throw new IllegalStateException("Unexpected value: " + roll);
+        }
+        return Weather.values()[roll];
     }
 }
