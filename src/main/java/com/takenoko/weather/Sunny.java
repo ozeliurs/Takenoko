b79e64067dfa089eee6ea65ca516920d497abb51
Ozeliurs <ozeliurs@gmail.com>
2023-01-13 23:51:38 +0100

    feat: implement sunny execute
    Ref. #269

--- src/main/java/com/takenoko/weather/Sunny.java
+++ src/main/java/com/takenoko/weather/Sunny.java
@@ -1,11 +1,11 @@
 package com.takenoko.weather;
 
 import com.takenoko.engine.Board;
-import com.takenoko.engine.BotState;
+import com.takenoko.engine.BotManager;
 
 public class Sunny implements Weather {
     @Override
-    public void execute(Board board, BotState botState) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public void execute(Board board, BotManager botManager) {
+        botManager.addAction();
     }
 }
