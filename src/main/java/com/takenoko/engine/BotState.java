fe3886aaf1c51d78340840094ebf290209c93ef3
Ozeliurs <ozeliurs@gmail.com>
2023-01-13 22:38:49 +0100

    feat: edit BotState to have availableActions
    Ref. #269

--- src/main/java/com/takenoko/engine/BotState.java
+++ src/main/java/com/takenoko/engine/BotState.java
@@ -1,8 +1,11 @@
 package com.takenoko.engine;
 
+import com.takenoko.actions.Action;
 import com.takenoko.inventory.Inventory;
 import com.takenoko.objective.BambooInInventoryObjective;
 import com.takenoko.objective.Objective;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class BotState { // DEFAULT VALUES
@@ -10,25 +13,47 @@ public class BotState { // DEFAULT VALUES
     private static final Objective DEFAULT_OBJECTIVE = new BambooInInventoryObjective(2);
     private final int numberOfActions;
     private Objective objective;
+    private final Inventory inventory;
+    private List<Class<? extends Action>> availableActions;
 
+    public BotState(
+            int numberOfActions,
+            Objective objective,
+            Inventory inventory,
+            List<Class<? extends Action>> availableActions) {
+        this.numberOfActions = numberOfActions;
+        this.objective = objective;
+        this.inventory = inventory;
+        this.availableActions = availableActions;
+    }
+
+    public BotState() {
+        this(DEFAULT_NUMBER_OF_ACTIONS, DEFAULT_OBJECTIVE, new Inventory(), new ArrayList<>());
+    }
+
+    public BotState(BotState botState) {
+        this.numberOfActions = botState.numberOfActions;
+        this.objective = botState.objective.copy();
+        this.inventory = botState.getInventory().copy();
+        this.availableActions = new ArrayList<>(botState.availableActions);
+    }
+
+    /**
+     * Get the current Objective of the bot
+     *
+     * @return Objective
+     */
     public Objective getObjective() {
         return objective;
     }
 
+    /**
+     * Set the current Objective of the bot
+     *
+     * @param objective the objective
+     */
     public void setObjective(Objective objective) {
         this.objective = objective;
-    }
-
-    private final Inventory inventory;
-
-    public BotState(int numberOfActions, Objective objective, Inventory inventory) {
-        this.numberOfActions = numberOfActions;
-        this.objective = objective;
-        this.inventory = inventory;
-    }
-
-    public BotState() {
-        this(DEFAULT_NUMBER_OF_ACTIONS, DEFAULT_OBJECTIVE, new Inventory());
     }
 
     /**
@@ -54,15 +79,27 @@ public class BotState { // DEFAULT VALUES
         return inventory;
     }
 
+    /**
+     * Return the list of available actions
+     *
+     * @return the list of available actions
+     */
+    public List<Class<? extends Action>> getAvailableActions() {
+        return availableActions;
+    }
+
+    /**
+     * Set the list of available actions
+     *
+     * @param availableActions the list of available actions
+     */
+    public void setAvailableActions(List<Class<? extends Action>> availableActions) {
+        this.availableActions = availableActions;
+    }
+
     public void reset() {
         this.objective.reset();
         this.inventory.clear();
-    }
-
-    public BotState(BotState botState) {
-        this.numberOfActions = botState.numberOfActions;
-        this.objective = botState.objective.copy();
-        this.inventory = botState.getInventory().copy();
     }
 
     public BotState copy() {
