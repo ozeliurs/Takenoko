4dd798f872bf210880889037338e891484aa4cf7
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-01-18 19:53:09 +0100

    feat: add a way to revert the Weather
    Ref: #269

--- src/main/java/com/takenoko/weather/Weather.java
+++ src/main/java/com/takenoko/weather/Weather.java
@@ -5,5 +5,7 @@ import com.takenoko.engine.BotManager;
 
 public interface Weather {
     /** Code to be executed when the weather is rolled and modifiers applied. */
-    void execute(Board board, BotManager botManager);
+    void apply(Board board, BotManager botManager);
+
+    void revert(Board board, BotManager botManager);
 }
