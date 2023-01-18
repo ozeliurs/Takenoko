7f43b38a61212191e88b88541d5e7e5bf22b44e8
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-01-18 23:49:08 +0100

    docs: add docs to ActionResult
    Ref: #269

--- src/main/java/com/takenoko/actions/ActionResult.java
+++ src/main/java/com/takenoko/actions/ActionResult.java
@@ -3,6 +3,12 @@ package com.takenoko.actions;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class represents the result of an action.
+ *
+ * @param availableActions the availableActions available to the bot after the action
+ * @param cost the cost of the action
+ */
 public record ActionResult(List<Class<? extends Action>> availableActions, int cost) {
     static List<Class<? extends Action>> DEFAULT_AVAILABLE_ACTIONS =
             List.of(MovePandaAction.class, MoveGardenerAction.class, PlaceTileAction.class);
