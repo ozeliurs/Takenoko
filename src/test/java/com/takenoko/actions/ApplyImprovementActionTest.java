351fc30c841e012e7f1e2405f749fdb241a392ee
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-01-20 00:16:01 +0100

    feat: add ApplyImprovementAction
    Ref: #285

---
+++ src/test/java/com/takenoko/actions/ApplyImprovementActionTest.java
@@ -0,0 +1,37 @@
+package com.takenoko.actions;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.*;
+
+import com.takenoko.engine.Board;
+import com.takenoko.engine.BotManager;
+import com.takenoko.inventory.Inventory;
+import com.takenoko.layers.tile.ImprovementType;
+import com.takenoko.vector.PositionVector;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class ApplyImprovementActionTest {
+
+    @Nested
+    @DisplayName("method execute")
+    class Execute {
+        @Test
+        @DisplayName("should throw IllegalStateException when improvement not in inventory")
+        void shouldThrowIllegalStateExceptionWhenImprovementNotInInventory() {
+            BotManager botManager = mock(BotManager.class);
+            Board board = mock(Board.class);
+            ApplyImprovementAction applyImprovementAction =
+                    new ApplyImprovementAction(
+                            mock(ImprovementType.class), mock(PositionVector.class));
+            when(botManager.getInventory()).thenReturn(mock(Inventory.class));
+            when(botManager.getInventory().hasImprovement(any(ImprovementType.class)))
+                    .thenReturn(false);
+
+            assertThatThrownBy(() -> applyImprovementAction.execute(board, botManager))
+                    .isInstanceOf(IllegalStateException.class)
+                    .hasMessage("Improvement not in inventory");
+        }
+    }
+}
