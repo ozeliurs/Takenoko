6c4cb38c4b41f1adf1ba523f5a4c6a5aece4bdfd
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-01-19 19:07:55 +0100

    fix: GetAndStoreImprovementActionTest
    Ref: #285

--- src/test/java/com/takenoko/actions/GetAndStoreImprovementActionTest.java
+++ src/test/java/com/takenoko/actions/GetAndStoreImprovementActionTest.java
@@ -1,11 +1,13 @@
 package com.takenoko.actions;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.takenoko.engine.Board;
 import com.takenoko.engine.BotManager;
+import com.takenoko.inventory.Inventory;
 import com.takenoko.layers.tile.ImprovementType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,11 +30,19 @@ class GetAndStoreImprovementActionTest {
     @DisplayName("execute")
     class Execute {
         @Test
-        @DisplayName("When getting an improvement, the number of actions consumed is 1")
+        @DisplayName(
+                "When getting an improvement and the improvement is available, the number of"
+                        + " actions consumed is 1")
         void whenGettingAnImprovementTheNumberOfActionsConsumedIs1() {
             GetAndStoreImprovementAction action =
                     new GetAndStoreImprovementAction(ImprovementType.FERTILIZER);
-            ActionResult result = action.execute(mock(Board.class), mock(BotManager.class));
+            Board board = mock(Board.class);
+            BotManager botManager = mock(BotManager.class);
+            when(board.hasImprovementInDeck(any(ImprovementType.class))).thenReturn(true);
+            when(board.drawImprovement(any(ImprovementType.class)))
+                    .thenReturn(mock(ImprovementType.class));
+            when(botManager.getInventory()).thenReturn(mock(Inventory.class));
+            ActionResult result = action.execute(board, botManager);
             assertEquals(1, result.cost());
         }
     }
