651ecf0a6c6af2b3aab24da4bd365d902401a6a0
Sayf <sayf-eddine.halmi@etu.unice.fr>
2023-01-19 16:42:29 +0100

    feat: add InventoryImprovements to Inventory
    Ref: #282

--- src/main/java/com/takenoko/inventory/Inventory.java
+++ src/main/java/com/takenoko/inventory/Inventory.java
@@ -4,25 +4,37 @@ import java.util.Objects;
 
 public class Inventory {
     private final InventoryBambooStack bambooStack;
+    private InventoryImprovements inventoryImprovements;
+
+    public Inventory(
+            InventoryBambooStack bambooStack, InventoryImprovements inventoryImprovements) {
+        this.bambooStack = bambooStack;
+        this.inventoryImprovements = inventoryImprovements;
+    }
 
     public Inventory(InventoryBambooStack bambooStack) {
-        this.bambooStack = bambooStack;
+        this(bambooStack, new InventoryImprovements());
     }
 
     public Inventory() {
-        this(new InventoryBambooStack(0));
+        this(new InventoryBambooStack(0), new InventoryImprovements());
     }
 
     public InventoryBambooStack getBambooStack() {
         return bambooStack;
     }
 
+    public InventoryImprovements getInventoryImprovements() {
+        return inventoryImprovements;
+    }
+
     public void clear() {
         bambooStack.clear();
+        inventoryImprovements.clear();
     }
 
     public Inventory copy() {
-        return new Inventory(bambooStack.copy());
+        return new Inventory(bambooStack.copy(), inventoryImprovements.copy());
     }
 
     @Override
@@ -30,11 +42,12 @@ public class Inventory {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Inventory inventory = (Inventory) o;
-        return getBambooStack().equals(inventory.getBambooStack());
+        return getBambooStack().equals(inventory.getBambooStack())
+                && inventoryImprovements.equals(inventory.inventoryImprovements);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getBambooStack());
+        return Objects.hash(getBambooStack(), inventoryImprovements);
     }
 }
