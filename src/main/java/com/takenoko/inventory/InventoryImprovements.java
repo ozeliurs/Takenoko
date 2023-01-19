70efaafa149c874c24f5b863e80fc741a2a20cb5
Sayf <sayf-eddine.halmi@etu.unice.fr>
2023-01-19 16:41:39 +0100

    feat: create InventoryImprovements
    Ref: #282

---
+++ src/main/java/com/takenoko/inventory/InventoryImprovements.java
@@ -0,0 +1,41 @@
+package com.takenoko.inventory;
+
+import com.takenoko.layers.tile.ImprovementType;
+import java.util.ArrayList;
+import java.util.List;
+
+public class InventoryImprovements extends ArrayList<ImprovementType> {
+    public InventoryImprovements() {
+        super();
+    }
+
+    public InventoryImprovements(List<ImprovementType> inventoryImprovements) {
+        this.addAll(inventoryImprovements);
+    }
+
+    /**
+     * @return copy of the stash of improvement stored by the bot
+     */
+    public InventoryImprovements copy() {
+        return new InventoryImprovements(this);
+    }
+
+    /**
+     * @param pickedImprovement store the chosen improvement in the inventory
+     */
+    public void store(ImprovementType pickedImprovement) {
+        super.add(pickedImprovement);
+    }
+
+    /**
+     * @param toUse
+     * remove used improvement from list or throw exception if not possessed
+     */
+    public void use(ImprovementType toUse) {
+        if (!contains(toUse)) {
+            throw new IllegalArgumentException(
+                    "You do not possess any improvements of type: " + toUse);
+        }
+        super.remove(toUse);
+    }
+}
