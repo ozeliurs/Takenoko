0c13047d8a9b40b776a32311bcf38b96f8e0c667
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-28 16:35:50 +0100

    feat: add MultipleGardenerObjective
    Refs: #304

---
+++ src/main/java/com/takenoko/objective/MultipleGardenerObjective.java
@@ -0,0 +1,63 @@
+package com.takenoko.objective;
+
+import com.takenoko.engine.Board;
+import com.takenoko.engine.BotManager;
+import com.takenoko.vector.PositionVector;
+import java.util.Comparator;
+
+public class MultipleGardenerObjective extends Objective {
+
+    SingleGardenerObjective objective;
+    private final int numberOfTimes;
+
+    public MultipleGardenerObjective(SingleGardenerObjective objective, int numberOfTimes) {
+        super(ObjectiveTypes.BAMBOO_STACK, ObjectiveState.NOT_ACHIEVED);
+        this.objective = objective;
+        this.numberOfTimes = numberOfTimes;
+    }
+
+    public MultipleGardenerObjective(MultipleGardenerObjective multipleGardenerObjective) {
+        this(multipleGardenerObjective.objective.copy(), multipleGardenerObjective.numberOfTimes);
+    }
+
+    @Override
+    public void verify(Board board, BotManager botManager) {
+        if (this.objective.getMatchingPositions(board).size() >= numberOfTimes) {
+            this.state = ObjectiveState.ACHIEVED;
+        }
+    }
+
+    @Override
+    public void reset() {
+        this.state = ObjectiveState.NOT_ACHIEVED;
+    }
+
+    @Override
+    public Objective copy() {
+        return new MultipleGardenerObjective(this);
+    }
+
+    @Override
+    public float getCompletion(Board board, BotManager botManager) {
+        return 1
+                - ((float)
+                                objective.getEligiblePositions(board).stream()
+                                        .filter(
+                                                v ->
+                                                        board.getBambooAt(v).getBambooCount()
+                                                                <= objective.getTargetSize())
+                                        .sorted(
+                                                Comparator.comparingInt(
+                                                                v ->
+                                                                        board.getBambooAt(
+                                                                                        (PositionVector)
+                                                                                                v)
+                                                                                .getBambooCount())
+                                                        .reversed())
+                                        .limit(numberOfTimes)
+                                        .map(v -> board.getBambooAt(v).getBambooCount())
+                                        .map(v -> Math.abs(v - objective.getTargetSize()))
+                                        .reduce(0, Integer::sum)
+                        / (numberOfTimes * objective.getTargetSize()));
+    }
+}
