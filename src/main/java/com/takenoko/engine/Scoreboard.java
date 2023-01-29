2527d5a08a48e62f6c4ae9156f3da130cfb79656
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-29 13:16:33 +0100

    style: apply style
    Refs: #311

--- src/main/java/com/takenoko/engine/Scoreboard.java
+++ src/main/java/com/takenoko/engine/Scoreboard.java
@@ -4,9 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * This class is used to store the number of win for each bot.
- */
+/** This class is used to store the number of win for each bot. */
 public class Scoreboard {
     private final HashMap<BotManager, Integer> numberOfVictoryHashMap;
 
