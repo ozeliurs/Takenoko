cd0ab7e78c8bfbadda0b387dd3f6295240b45148
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-30 10:59:08 +0100

    feat: add PatternFactory.DIAMOND
    Refs: #312

---
+++ src/main/java/com/takenoko/shape/Diamond.java
@@ -0,0 +1,16 @@
+package com.takenoko.shape;
+
+import com.takenoko.layers.tile.Tile;
+import com.takenoko.layers.tile.TileColor;
+import com.takenoko.vector.PositionVector;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class Diamond extends Pattern {
+    public Diamond() {
+        super(
+                Pair.of(new PositionVector(0, 0, 0), new Tile(TileColor.ANY)),
+                Pair.of(new PositionVector(1, 0, -1), new Tile(TileColor.ANY)),
+                Pair.of(new PositionVector(0, 1, -1), new Tile(TileColor.ANY)),
+                Pair.of(new PositionVector(-1, 1, 0), new Tile(TileColor.ANY)));
+    }
+}
