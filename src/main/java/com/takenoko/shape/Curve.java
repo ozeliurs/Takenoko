7f9eb0ee8e8176fd489e437dd2e2f10e4d37bb2b
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-30 10:59:03 +0100

    feat: add PatternFactory.CURVE
    Refs: #312

---
+++ src/main/java/com/takenoko/shape/Curve.java
@@ -0,0 +1,15 @@
+package com.takenoko.shape;
+
+import com.takenoko.layers.tile.Tile;
+import com.takenoko.layers.tile.TileColor;
+import com.takenoko.vector.PositionVector;
+import org.apache.commons.lang3.tuple.Pair;
+
+public class Curve extends Pattern {
+    public Curve() {
+        super(
+                Pair.of(new PositionVector(0, 0, 0), new Tile(TileColor.ANY)),
+                Pair.of(new PositionVector(1, 0, -1), new Tile(TileColor.ANY)),
+                Pair.of(new PositionVector(2, -1, -1), new Tile(TileColor.ANY)));
+    }
+}
