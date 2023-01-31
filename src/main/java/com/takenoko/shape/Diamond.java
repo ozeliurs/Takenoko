08035596b813ae5a5166e415a6a1ac72c10d89f7
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-31 14:01:09 +0100

    feat: DIAMOND constructor
    Refs: #312

--- src/main/java/com/takenoko/shape/Diamond.java
+++ src/main/java/com/takenoko/shape/Diamond.java
@@ -5,12 +5,13 @@ import com.takenoko.layers.tile.TileColor;
 import com.takenoko.vector.PositionVector;
 import org.apache.commons.lang3.tuple.Pair;
 
+/** Build a solid color Diamond pattern */
 public class Diamond extends Pattern {
-    public Diamond() {
+    public Diamond(TileColor color) {
         super(
-                Pair.of(new PositionVector(0, 0, 0), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(1, 0, -1), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(0, 1, -1), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(-1, 1, 0), new Tile(TileColor.ANY)));
+                Pair.of(new PositionVector(0, 0, 0), new Tile(color)),
+                Pair.of(new PositionVector(1, 0, -1), new Tile(color)),
+                Pair.of(new PositionVector(0, 1, -1), new Tile(color)),
+                Pair.of(new PositionVector(-1, 1, 0), new Tile(color)));
     }
 }
