3a8b5a8d1a8cb93d92bbac3dc2fefe646cad508d
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-31 14:01:00 +0100

    feat: LINE constructor
    Refs: #312

--- src/main/java/com/takenoko/shape/Line.java
+++ src/main/java/com/takenoko/shape/Line.java
@@ -6,10 +6,10 @@ import com.takenoko.vector.PositionVector;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class Line extends Pattern {
-    public Line() {
+    public Line(TileColor color) {
         super(
-                Pair.of(new PositionVector(0, 0, 0), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(1, 0, -1), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(2, 0, -2), new Tile(TileColor.ANY)));
+                Pair.of(new PositionVector(0, 0, 0), new Tile(color)),
+                Pair.of(new PositionVector(1, 0, -1), new Tile(color)),
+                Pair.of(new PositionVector(2, 0, -2), new Tile(color)));
     }
 }
