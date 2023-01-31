f4142833889fd8a2c50a06d0550bad9ef04064f4
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-31 14:01:34 +0100

    feat: TRIANGLE constructor
    Refs: #312

--- src/main/java/com/takenoko/shape/Triangle.java
+++ src/main/java/com/takenoko/shape/Triangle.java
@@ -6,10 +6,10 @@ import com.takenoko.vector.PositionVector;
 import org.apache.commons.lang3.tuple.Pair;
 
 public class Triangle extends Pattern {
-    public Triangle() {
+    public Triangle(TileColor tileColor) {
         super(
-                Pair.of(new PositionVector(0, 0, 0), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(1, 0, -1), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(0, 1, -1), new Tile(TileColor.ANY)));
+                Pair.of(new PositionVector(0, 0, 0), new Tile(tileColor)),
+                Pair.of(new PositionVector(1, 0, -1), new Tile(tileColor)),
+                Pair.of(new PositionVector(0, 1, -1), new Tile(tileColor)));
     }
 }
