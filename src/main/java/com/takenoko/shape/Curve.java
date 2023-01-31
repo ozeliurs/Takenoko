b2271def4dab28b0a37a39ebbcdb89b0ef8d141a
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-01-31 14:01:14 +0100

    feat: CURVE constructor
    Refs: #312

--- src/main/java/com/takenoko/shape/Curve.java
+++ src/main/java/com/takenoko/shape/Curve.java
@@ -5,11 +5,12 @@ import com.takenoko.layers.tile.TileColor;
 import com.takenoko.vector.PositionVector;
 import org.apache.commons.lang3.tuple.Pair;
 
+/** Build a solid color Curve pattern */
 public class Curve extends Pattern {
-    public Curve() {
+    public Curve(TileColor tileColor) {
         super(
-                Pair.of(new PositionVector(0, 0, 0), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(1, 0, -1), new Tile(TileColor.ANY)),
-                Pair.of(new PositionVector(2, -1, -1), new Tile(TileColor.ANY)));
+                Pair.of(new PositionVector(0, 0, 0), new Tile(tileColor)),
+                Pair.of(new PositionVector(1, 0, -1), new Tile(tileColor)),
+                Pair.of(new PositionVector(2, -1, -1), new Tile(tileColor)));
     }
 }
