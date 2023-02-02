0e91e76c62e628fd93e1255c47afafa76a295b3a
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-02-02 11:45:05 +0100

    feat: add IrrigationChannel
    ref: #337

--- src/main/java/com/takenoko/layers/irrigation/IrrigationChannel.java
+++ src/main/java/com/takenoko/layers/irrigation/IrrigationChannel.java
@@ -1,23 +1,3 @@
 package com.takenoko.layers.irrigation;
 
-import com.takenoko.vector.PositionVector;
-import org.jetbrains.annotations.NotNull;
-
-/**
- * This record represents an irrigation channel.
- *
- * @param leftTilePosition the position of the tile on the left of the irrigation channel
- * @param rightTilePosition the position of the tile on the right of the irrigation channel
- */
-public record IrrigationChannel(
-        @NotNull PositionVector leftTilePosition, @NotNull PositionVector rightTilePosition) {
-    public IrrigationChannel {
-        if (leftTilePosition.equals(rightTilePosition)) {
-            throw new IllegalArgumentException(
-                    "The irrigation channel positions cannot be the same");
-        }
-        if (leftTilePosition.distance(rightTilePosition) != 1) {
-            throw new IllegalArgumentException("The irrigation channel position must be adjacent");
-        }
-    }
-}
+public class IrrigationChannel {}
