ff802ce601237b03a5737bfbce02d3a637600c4b
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-02-02 11:20:15 +0100

    docs: add Javadoc to IrrigationChannel
    ref: #337

--- src/main/java/com/takenoko/layers/irrigation/IrrigationChannel.java
+++ src/main/java/com/takenoko/layers/irrigation/IrrigationChannel.java
@@ -3,6 +3,12 @@ package com.takenoko.layers.irrigation;
 import com.takenoko.vector.PositionVector;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * This record represents an irrigation channel.
+ *
+ * @param leftTilePosition the position of the tile on the left of the irrigation channel
+ * @param rightTilePosition the position of the tile on the right of the irrigation channel
+ */
 public record IrrigationChannel(
         @NotNull PositionVector leftTilePosition, @NotNull PositionVector rightTilePosition) {
     public IrrigationChannel {
