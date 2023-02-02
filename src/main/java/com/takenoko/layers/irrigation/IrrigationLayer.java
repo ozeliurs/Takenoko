83c01845530eb078326dcc18aafa41019fa65416
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-02-02 22:36:45 +0100

    feat: add irrigation to TileLayer
    ref: #338

--- src/main/java/com/takenoko/layers/irrigation/IrrigationLayer.java
+++ src/main/java/com/takenoko/layers/irrigation/IrrigationLayer.java
@@ -2,42 +2,91 @@ package com.takenoko.layers.irrigation;
 
 import com.takenoko.engine.Board;
 import com.takenoko.vector.PositionVector;
-import com.takenoko.vector.Vector;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 public class IrrigationLayer {
-    Map<PositionVector, Boolean> irrigation = new HashMap<>();
-    Map<IrrigationChannelPosition, IrrigationChannel> irrigationChannels = new HashMap<>();
-    HashSet<IrrigationChannelPosition> availableIrrigationChannelPositions = new HashSet<>();
+    private Map<PositionVector, Boolean> irrigation = new HashMap<>();
+    private Set<EdgePosition> irrigationChannelsPositions = new HashSet<>();
+    private HashSet<EdgePosition> availableEdgePositions = new HashSet<>();
 
-    public void irrigate(PositionVector positionVector) {
-        irrigation.put(positionVector, true);
+    public IrrigationLayer() {
+        irrigation.put(new PositionVector(0, 0, 0), true);
+        (new PositionVector(0, 0, 0))
+                .getNeighbors()
+                .forEach(positionVector -> irrigation.put(positionVector.toPositionVector(), true));
     }
 
-    public void placeIrrigation(
-            IrrigationChannelPosition irrigationChannelPosition,
-            IrrigationChannel irrigationChannel,
-            Board board) {
-        if (!availableIrrigationChannelPositions.contains(irrigationChannelPosition)) {
+    public IrrigationLayer(IrrigationLayer irrigationLayer) {
+        irrigation = new HashMap<>(irrigationLayer.irrigation);
+        irrigationChannelsPositions = new HashSet<>(irrigationLayer.irrigationChannelsPositions);
+        availableEdgePositions = new HashSet<>(irrigationLayer.availableEdgePositions);
+    }
+
+    public void placeIrrigation(EdgePosition edgePosition, Board board) {
+        if (!availableEdgePositions.contains(edgePosition)) {
             throw new IllegalArgumentException("The irrigation channel position is not available");
         }
-        irrigationChannels.put(irrigationChannelPosition, irrigationChannel);
+        irrigationChannelsPositions.add(edgePosition);
+        irrigation.put(edgePosition.getLeftTilePosition(), true);
+        irrigation.put(edgePosition.getRightTilePosition(), true);
+        updateAvailableIrrigationChannelPositions(edgePosition, board);
+    }
+
+    private void updateAvailableIrrigationChannelPositions(EdgePosition edgePosition, Board board) {
+        availableEdgePositions.remove(edgePosition);
+        updateAvailableIrrigationChannelPositions(edgePosition.getNeighbours().stream(), board);
     }
 
     public void updateAvailableIrrigationChannelPositions(
             PositionVector positionOfTilePlaced, Board board) {
         // an irrigation channel can be placed if there is an irrigation channel connected to it or
         // if it is connected to the pond
-        List<Vector> neighbours = positionOfTilePlaced.getNeighbors();
-        for (Vector neighbour : neighbours) {
-            if (irrigation.containsKey(neighbour.toPositionVector())) {
-                availableIrrigationChannelPositions.add(
-                        new IrrigationChannelPosition(
-                                positionOfTilePlaced, neighbour.toPositionVector()));
-            }
-        }
+        updateAvailableIrrigationChannelPositions(
+                EdgePosition.getEdgePositions(positionOfTilePlaced).stream(), board);
+    }
+
+    private void updateAvailableIrrigationChannelPositions(
+            Stream<EdgePosition> edgePositionStream, Board board) {
+        edgePositionStream
+                .filter(edgePosition -> !irrigationChannelsPositions.contains(edgePosition))
+                .filter(
+                        edgePosition ->
+                                board.isTile(edgePosition.getLeftTilePosition())
+                                        && board.isTile(edgePosition.getRightTilePosition()))
+                .filter(
+                        edgePosition ->
+                                edgePosition.getNeighbours().stream()
+                                        .anyMatch(
+                                                o ->
+                                                        irrigationChannelsPositions.contains(o)
+                                                                || EdgePosition.getEdgePositions(
+                                                                                new PositionVector(
+                                                                                        0, 0, 0))
+                                                                        .contains(o)))
+                .filter(
+                        edgePosition ->
+                                !EdgePosition.getEdgePositions(new PositionVector(0, 0, 0))
+                                        .contains(edgePosition))
+                .forEach(availableEdgePositions::add);
+    }
+
+    public IrrigationLayer copy() {
+        return new IrrigationLayer(this);
+    }
+
+    public Set<EdgePosition> getAvailableEdgePositions() {
+        return new HashSet<>(availableEdgePositions);
+    }
+
+    public Set<EdgePosition> getIrrigationChannelsPositions() {
+        return new HashSet<>(irrigationChannelsPositions);
+    }
+
+    public boolean isIrrigated(PositionVector positionVector) {
+        return irrigation.get(positionVector);
     }
 }
