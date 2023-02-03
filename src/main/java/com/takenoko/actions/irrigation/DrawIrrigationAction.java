cb53467528825557a9bf03ee7e8f2a6c32e3f87a
Apoorva Srinivas Appadoo <apoorva-srinivas.appadoo@etu.univ-cotedazur.fr>
2023-02-03 23:08:49 +0100

    refactor: remove unnecessary check in DrawIrrigationAction
    ref: #348

--- src/main/java/com/takenoko/actions/irrigation/DrawIrrigationAction.java
+++ src/main/java/com/takenoko/actions/irrigation/DrawIrrigationAction.java
@@ -12,11 +12,6 @@ public class DrawIrrigationAction implements Action {
 
     @Override
     public ActionResult execute(Board board, BotManager botManager) {
-        if (!board.hasIrrigation()) {
-            botManager.displayMessage("No more irrigation in deck");
-            return new ActionResult(0);
-        }
-
         board.drawIrrigation();
         botManager.getInventory().collectIrrigationChannel();
         return new ActionResult(1);
