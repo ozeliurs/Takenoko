e83bcb1bf9141b369eb62c7af55d09475466de73
Ozeliurs <ozeliurs@gmail.com>
2023-02-04 23:57:22 +0100

    feat: when the gardener moves, he grows bamboo all arround
    Ref. #358

--- src/main/java/com/takenoko/actors/Gardener.java
+++ src/main/java/com/takenoko/actors/Gardener.java
@@ -3,6 +3,7 @@ package com.takenoko.actors;
 import com.takenoko.engine.Board;
 import com.takenoko.layers.bamboo.LayerBambooStack;
 import com.takenoko.vector.PositionVector;
+import com.takenoko.vector.Vector;
 
 /** Gardener class. The gardener is an actor that can move on the board. */
 public class Gardener extends com.takenoko.actors.Actor {
@@ -29,14 +30,28 @@ public class Gardener extends com.takenoko.actors.Actor {
     }
 
     public LayerBambooStack afterMove(Board board) {
-        // check if the gardener can grow bamboo (not pond)
-        if (board.isBambooGrowableAt(this.getPositionVector())) {
-            // grow bamboo
-            board.growBamboo(this.getPositionVector());
+
+        // check if the gardener can grow bamboo on adjacent tiles
+        for (Vector adjacent : this.getPositionVector().getNeighbors()) {
+            tryToGrowBambooAt(board, adjacent.toPositionVector());
+        }
+
+        // check if the gardener can grow bamboo on the tile he's on
+        if (tryToGrowBambooAt(board, this.getPositionVector())) {
             return new LayerBambooStack(1);
         } else {
             return new LayerBambooStack(0);
         }
+    }
+
+    private boolean tryToGrowBambooAt(Board board, PositionVector position) {
+        // check if the gardener can grow bamboo
+        if (board.isBambooGrowableAt(position)) {
+            // grow bamboo
+            board.growBamboo(position);
+            return true;
+        }
+        return false;
     }
 
     public Gardener copy() {
