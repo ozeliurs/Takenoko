747e8380eb26a617d1e6b46167e0a10fbd9a0d54
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-02-05 22:47:15 +0100

    refactor: extract management of action from BotState to ActionManager
    Refs: #376

--- src/main/java/com/takenoko/engine/BotState.java
+++ src/main/java/com/takenoko/engine/BotState.java
@@ -2,60 +2,30 @@ package com.takenoko.engine;
 
 import com.takenoko.actions.Action;
 import com.takenoko.actions.ActionResult;
-import com.takenoko.actions.DefaultAction;
-import com.takenoko.actions.actors.MoveGardenerAction;
-import com.takenoko.actions.actors.MovePandaAction;
-import com.takenoko.actions.annotations.ActionAnnotation;
-import com.takenoko.actions.annotations.ActionCanBePlayedMultipleTimesPerTurn;
-import com.takenoko.actions.annotations.ActionType;
-import com.takenoko.actions.improvement.ApplyImprovementFromInventoryAction;
-import com.takenoko.actions.irrigation.DrawIrrigationAction;
-import com.takenoko.actions.irrigation.PlaceIrrigationFromInventoryAction;
-import com.takenoko.actions.objective.DrawObjectiveAction;
-import com.takenoko.actions.objective.RedeemObjectiveAction;
-import com.takenoko.actions.tile.DrawTileAction;
 import com.takenoko.inventory.Inventory;
 import com.takenoko.objective.Objective;
-import com.takenoko.weather.Windy;
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Objects;
 
-/** This class is used to store the state of a bot. */
+/**
+ * This class is used to store the state of a bot.
+ */
 public class BotState {
-    private static final int DEFAULT_NUMBER_OF_ACTIONS = 2;
     public static final int MAX_OBJECTIVES = 5;
     private final ObjectiveManager objectiveManager;
-
-    private int numberOfActions;
+    private final ActionManager actionManager;
     private final Inventory inventory;
-    private List<Class<? extends Action>> availableActions;
-    private List<Class<? extends Action>> alreadyDoneActions = new ArrayList<>();
-
-    private static final List<Class<? extends DefaultAction>> DEFAULT_AVAILABLE_ACTIONS =
-            List.of(
-                    DrawObjectiveAction.class,
-                    DrawTileAction.class,
-                    DrawIrrigationAction.class,
-                    MoveGardenerAction.class,
-                    MovePandaAction.class,
-                    RedeemObjectiveAction.class,
-                    PlaceIrrigationFromInventoryAction.class,
-                    ApplyImprovementFromInventoryAction.class);
 
     public BotState(
-            int numberOfActions,
-            Inventory inventory,
-            List<Class<? extends Action>> availableActions,
-            ObjectiveManager objectiveManager) {
-        this.numberOfActions = numberOfActions;
+            Inventory inventory, ObjectiveManager objectiveManager, ActionManager actionManager) {
         this.inventory = inventory;
-        this.availableActions = availableActions;
         this.objectiveManager = objectiveManager;
+        this.actionManager = actionManager;
     }
 
     public BotState() {
-        this(DEFAULT_NUMBER_OF_ACTIONS, new Inventory(), new ArrayList<>(), new ObjectiveManager());
+        this(new Inventory(), new ObjectiveManager(), new ActionManager());
     }
 
     /**
@@ -64,10 +34,8 @@ public class BotState {
      * @param botState the state to copy
      */
     public BotState(BotState botState) {
-        this.numberOfActions = botState.numberOfActions;
         this.inventory = botState.getInventory().copy();
-        this.availableActions = new ArrayList<>(botState.availableActions);
-        this.alreadyDoneActions = new ArrayList<>(botState.alreadyDoneActions);
+        this.actionManager = new ActionManager(botState.actionManager);
         this.objectiveManager = new ObjectiveManager(botState.objectiveManager);
     }
 
@@ -75,13 +43,13 @@ public class BotState {
     // ------------------- OBJECT METHODS ------------------- //
     // ------------------------------------------------------ //
 
-    /** reset everything to the default values */
+    /**
+     * reset everything to the default values
+     */
     public void reset() {
         this.objectiveManager.reset();
+        this.actionManager.reset();
         this.inventory.clear();
-        this.numberOfActions = DEFAULT_NUMBER_OF_ACTIONS;
-        this.availableActions = new ArrayList<>();
-        this.alreadyDoneActions.clear();
     }
 
     /**
@@ -98,23 +66,27 @@ public class BotState {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         BotState botState = (BotState) o;
-        return getNumberOfActions() == botState.getNumberOfActions()
-                && getObjectives().equals(botState.getObjectives())
-                && getInventory().equals(botState.getInventory())
-                && getObjectiveScore() == botState.getObjectiveScore()
-                && getAchievedObjectives().equals(botState.getAchievedObjectives())
-                && getObjectiveManager().equals(botState.getObjectiveManager());
+        return getInventory().equals(botState.getInventory())
+                && getObjectiveManager().equals(botState.getObjectiveManager())
+                && getActionManager().equals(botState.getActionManager());
     }
 
     @Override
     public int hashCode() {
         return Objects.hash(
-                getNumberOfActions(),
-                getObjectives(),
                 getInventory(),
-                getAchievedObjectives(),
-                getObjectiveScore(),
-                getObjectiveManager());
+                getObjectiveManager(),
+                getActionManager());
+    }
+
+    /**
+     * update the objectives
+     *
+     * @param board the board
+     */
+    public void update(Board board) {
+        this.objectiveManager.updateObjectives(board, this);
+        this.actionManager.updateDefaultActions(board, this);
     }
 
     // ------------------------------------------------------ //
@@ -141,17 +113,20 @@ public class BotState {
     // -------------- METHOD RELATED TO ACTIONS ------------- //
     // ------------------------------------------------------ //
 
+    private ActionManager getActionManager() {
+        return actionManager;
+    }
+
     public void setNumberOfActions(int numberOfActions) {
-        this.numberOfActions = numberOfActions;
+        this.actionManager.setNumberOfActions(numberOfActions);
     }
 
     /**
      * @return number of actions the bot can do in a turn
      */
     protected int getNumberOfActions() {
-        return numberOfActions;
+        return this.actionManager.getNumberOfActions();
     }
-
 
     /**
      * Return the list of available actions. If actions of FORCED type are available, only these
@@ -160,28 +135,7 @@ public class BotState {
      * @return the list of available actions
      */
     public List<Class<? extends Action>> getAvailableActions() {
-        List<Class<? extends Action>> forcedActions =
-                availableActions.stream()
-                        .filter(
-                                action ->
-                                        action.getAnnotation(ActionAnnotation.class).value()
-                                                == ActionType.FORCED)
-                        .toList();
-
-        if (forcedActions.isEmpty()) {
-            return availableActions;
-        } else {
-            return forcedActions;
-        }
-    }
-
-    /**
-     * Set the list of available actions
-     *
-     * @param availableActions the list of available actions
-     */
-    public void setAvailableActions(List<Class<? extends Action>> availableActions) {
-        this.availableActions = availableActions;
+        return this.actionManager.getAvailableActions();
     }
 
     /**
@@ -190,7 +144,7 @@ public class BotState {
      * @param action the action to add
      */
     public void addAvailableAction(Class<? extends Action> action) {
-        this.availableActions.add(action);
+        this.actionManager.addAvailableAction(action);
     }
 
     /**
@@ -199,66 +153,24 @@ public class BotState {
      * @param actions the list of actions to add
      */
     public void addAvailableActions(List<Class<? extends Action>> actions) {
-        this.availableActions.addAll(actions);
+        this.actionManager.addAvailableActions(actions);
     }
 
-    /** add an action to the number of actions to plau this turn */
+    /**
+     * add an action to the number of actions to plau this turn
+     */
     public void addAction() {
-        numberOfActions++;
-    }
-
-    /** clear the list of available actions of the FORCED type */
-    private void clearForcedActions() {
-        availableActions.removeIf(
-                action ->
-                        action.getAnnotation(ActionAnnotation.class).value() == ActionType.FORCED);
+        this.actionManager.addAction();
     }
 
     /**
      * update an action in available actions
      *
-     * @param action the action to update
+     * @param action       the action to update
      * @param actionResult the result of the action
      */
     public void updateAvailableActions(Action action, ActionResult actionResult) {
-        this.availableActions.remove(action.getClass());
-        this.alreadyDoneActions.add(action.getClass());
-        this.clearForcedActions();
-        this.addAvailableActions(actionResult.availableActions());
-        this.setNumberOfActions(this.getNumberOfActions() - actionResult.cost());
-    }
-
-    /**
-     * update the objectives
-     *
-     * @param board the board
-     * @param botManager the bot manager
-     */
-    public void update(Board board, BotManager botManager) {
-        this.objectiveManager.updateObjectives(board, botManager);
-        updateDefaultActions(board);
-    }
-
-    /**
-     * update to the defaults actions
-     *
-     * @param board the board
-     */
-    private void updateDefaultActions(Board board) {
-        availableActions.removeIf(
-                action ->
-                        action.getAnnotation(ActionAnnotation.class).value() == ActionType.DEFAULT);
-        DEFAULT_AVAILABLE_ACTIONS.stream()
-                .filter(
-                        actionClass ->
-                                DefaultAction.canBePlayed(board, this, actionClass)
-                                        && (actionClass.isAnnotationPresent(
-                                                        ActionCanBePlayedMultipleTimesPerTurn.class)
-                                                || !alreadyDoneActions.contains(actionClass)
-                                                || board.getWeather()
-                                                        .map(v -> v.getClass().equals(Windy.class))
-                                                        .orElse(false)))
-                .forEach(actionClass -> availableActions.add(actionClass));
+        this.actionManager.updateAvailableActions(action, actionResult);
     }
 
     /**
@@ -267,9 +179,7 @@ public class BotState {
      * @param board the board
      */
     public void resetAvailableActions(Board board) {
-        this.availableActions = new ArrayList<>(DEFAULT_AVAILABLE_ACTIONS);
-        this.alreadyDoneActions = new ArrayList<>();
-        updateDefaultActions(board);
+        this.actionManager.resetAvailableActions(board, this);
     }
 
     /**
@@ -278,7 +188,7 @@ public class BotState {
      * @return the list of already done actions
      */
     public List<Class<? extends Action>> getAlreadyDoneActions() {
-        return new ArrayList<>(alreadyDoneActions);
+        return this.actionManager.getAlreadyDoneActions();
     }
 
     // ------------------------------------------------------ //
@@ -287,6 +197,7 @@ public class BotState {
 
     /**
      * Get the objective manager
+     *
      * @return ObjectiveManager
      */
     private ObjectiveManager getObjectiveManager() {
@@ -342,10 +253,9 @@ public class BotState {
      * for each objective, check if it is achieved
      *
      * @param board the board
-     * @param botManager the bot manager
      */
-    public void verifyObjectives(Board board, BotManager botManager) {
-        objectiveManager.verifyObjectives(board, botManager);
+    public void verifyObjectives(Board board) {
+        objectiveManager.verifyObjectives(board, this);
     }
 
     /**
