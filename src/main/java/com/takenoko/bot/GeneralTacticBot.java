a38b8ecbd8fae9b85be53068d3f99fefb396920f
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-02-06 05:53:06 +0100

    feat: add and repair bot
    Refs: #387 #386 #388

--- src/main/java/com/takenoko/bot/GeneralTacticBot.java
+++ src/main/java/com/takenoko/bot/GeneralTacticBot.java
@@ -3,9 +3,12 @@ package com.takenoko.bot;
 import static com.takenoko.bot.irrigation.pathfinding.IrrigationPathFinding.getShortestIrrigationPath;
 
 import com.takenoko.actions.Action;
+import com.takenoko.actions.irrigation.DrawIrrigationAction;
 import com.takenoko.actions.irrigation.PlaceIrrigationFromInventoryAction;
+import com.takenoko.actions.irrigation.StoreIrrigationInInventoryAction;
 import com.takenoko.actions.objective.RedeemObjectiveAction;
 import com.takenoko.actions.weather.ChooseAndApplyWeatherAction;
+import com.takenoko.actions.weather.ChooseIfApplyWeatherAction;
 import com.takenoko.engine.*;
 import com.takenoko.layers.irrigation.EdgePosition;
 import com.takenoko.layers.tile.Tile;
@@ -14,8 +17,10 @@ import com.takenoko.objective.Objective;
 import com.takenoko.objective.PandaObjective;
 import com.takenoko.objective.PatternObjective;
 import com.takenoko.shape.Shape;
+import com.takenoko.ui.ConsoleUserInterface;
 import com.takenoko.vector.PositionVector;
 import com.takenoko.weather.WeatherFactory;
+
 import java.util.*;
 
 public class GeneralTacticBot implements Bot {
@@ -23,42 +28,74 @@ public class GeneralTacticBot implements Bot {
     private final FullRandomBot fullRandomBot = new FullRandomBot();
     private final List<EdgePosition> irrigationToPlace = new ArrayList<>();
     private static final int ARBITRARY_MARGIN = 5;
+    private static final ConsoleUserInterface console = new ConsoleUserInterface();
 
     @Override
     public Action chooseAction(Board board, BotState botState, History history) {
+        /*
+         * Intelligence to complete a PatternObjective by adding irrigation channels
+         */
+        if (botState.getAvailableActions().contains(PlaceIrrigationFromInventoryAction.class)) {
+            console.displayDebug("BIG BRAIN MODE - PlaceIrrigationFromInventoryAction");
+            // Because it is empty, we have to analyze the board to find the best irrigation path
+            if (irrigationToPlace.isEmpty()) {
+                console.displayDebug("BIG BRAIN MODE - analyzed irrigation");
+                analyzeIrrigationToPlaceToCompletePatternObjective(board, botState);
+            }
+            // If there is a path to complete the objective, we place the first edge
+            if (!irrigationToPlace.isEmpty()) {
+                console.displayDebug("BIG BRAIN MODE - chose irrigationToPlace");
+                return new PlaceIrrigationFromInventoryAction(irrigationToPlace.remove(0));
+            }
+        }
+
+        /*
+         * Always apply the weather
+         */
+        if (botState.getAvailableActions().contains(ChooseIfApplyWeatherAction.class)) {
+            console.displayDebug("BIG BRAIN MODE - ChooseIfApplyWeatherAction");
+            return new ChooseIfApplyWeatherAction(true);
+        }
+
         /*
          * Intelligence when having the choice of the meteo to either :
          * - Choose the sun because it is the best move
          * - Choose the wind because it allows the bot to win next move <- WIP
          */
         if (botState.getAvailableActions().contains(ChooseAndApplyWeatherAction.class)) {
+            console.displayDebug("BIG BRAIN MODE - ChooseAndApplyWeatherAction");
             return new ChooseAndApplyWeatherAction(WeatherFactory.SUNNY.createWeather());
         }
 
         /*
-         * Intelligence to complete a PatternObjective by adding irrigation channels
+         * Always store the irrigation channel
          */
-        if (botState.getAvailableActions().contains(PlaceIrrigationFromInventoryAction.class)) {
-            // Because it is empty, we have to analyze the board to find the best irrigation path
-            if (irrigationToPlace.isEmpty()) {
-                analyzeIrrigationToPlaceToCompletePatternObjective(board, botState);
-            }
-            // If there is a path to complete the objective, we place the first edge
-            if (!irrigationToPlace.isEmpty()) {
-                return new PlaceIrrigationFromInventoryAction(irrigationToPlace.remove(0));
-            }
+        if (botState.getAvailableActions().contains(StoreIrrigationInInventoryAction.class)) {
+            console.displayDebug("BIG BRAIN MODE - StoreIrrigationInInventoryAction");
+            return new StoreIrrigationInInventoryAction();
         }
 
         /*
          * Intelligence to not redeem a PandaObjective if it is the last one in his hand, except if that makes it win
          */
         if (botState.getAvailableActions().contains(RedeemObjectiveAction.class)) {
+            console.displayDebug("BIG BRAIN MODE - RedeemObjectiveAction");
             RedeemObjectiveAction result = analyzeObjectivesToRedeem(botState, history);
             if (result != null) {
+                console.displayDebug("BIG BRAIN MODE - chose to redeem an objective");
                 return result;
             }
         }
 
+        /*
+         * Always draw an irrigation if nothing else worked
+         */
+        if (botState.getAvailableActions().contains(DrawIrrigationAction.class)) {
+            console.displayDebug("BIG BRAIN MODE - DrawIrrigationAction");
+            return new DrawIrrigationAction();
+        }
+
+        console.displayMessage("NOT SO BIG BRAIN MODE - LET THE FULL RANDOM BOT DO THE JOB");
         return fullRandomBot.chooseAction(board, botState, history);
     }
 
@@ -97,8 +134,8 @@ public class GeneralTacticBot implements Bot {
     /**
      * This method is used to calculate if the bot can win by redeeming its last panda objective
      *
-     * @param botState the bot state
-     * @param history the history
+     * @param botState       the bot state
+     * @param history        the history
      * @param pandaObjective the panda objective to redeem
      * @return true if it is worth redeeming the panda objective, false otherwise
      */
@@ -141,7 +178,7 @@ public class GeneralTacticBot implements Bot {
      * This method will add the edge position to the irrigationToPlace list so that the bot knows
      * that he has to place them in order to complete is pattern objectiv
      *
-     * @param board the board
+     * @param board    the board
      * @param botState the bot state
      */
     public void analyzeIrrigationToPlaceToCompletePatternObjective(Board board, BotState botState) {
@@ -151,28 +188,31 @@ public class GeneralTacticBot implements Bot {
         // - at least one pattern objective that is not already completed
         if (!allPatternObjectivesAreCompleted(botState)) {
             // Get all the shapes that could be completed by placing an irrigation
-            List<Shape> candidateShapes = getCandidateShape(board, botState);
+            List<List<Shape>> candidateShapes = getCandidateShapes(board, botState);
+            console.displayDebug("BIG BRAIN MODE - candidateShapes: " + candidateShapes);
 
             // If it is not empty, be intelligent
             if (!candidateShapes.isEmpty()) {
                 List<List<EdgePosition>> allPossiblePaths = new ArrayList<>();
-                for (Shape shape : candidateShapes) {
-                    // Get the position vector and tile of the shape
-                    Map<PositionVector, Tile> map = shape.getElements();
-                    // Find the tiles that are not irrigated yet
-                    List<PositionVector> candidateTilesToIrrigate =
-                            map.keySet().stream()
-                                    .filter(tile -> !board.isIrrigatedAt(tile))
-                                    .toList();
-                    // Find which irrigation to place to irrigate all the tiles of the shape
-                    List<EdgePosition> possibleEdgePositions =
-                            getShortestIrrigationPath(candidateTilesToIrrigate, board);
+                for (List<Shape> shapeList : candidateShapes) {
+                    for (Shape shape : shapeList) {
+                        // Get the position vector and tile of the shape
+                        Map<PositionVector, Tile> map = shape.getElements();
+                        // Find the tiles that are not irrigated yet
+                        List<PositionVector> candidateTilesToIrrigate =
+                                map.keySet().stream()
+                                        .filter(tile -> !board.isIrrigatedAt(tile))
+                                        .toList();
+                        // Find which irrigation to place to irrigate all the tiles of the shape
+                        List<EdgePosition> possibleEdgePositions =
+                                getShortestIrrigationPath(candidateTilesToIrrigate, board);
 
-                    // Verify that the number of irrigation needed does not exceed the number of
-                    // irrigation available
-                    if (botState.getInventory().getIrrigationChannelsCount()
-                            > possibleEdgePositions.size()) {
-                        allPossiblePaths.add(possibleEdgePositions);
+                        // Verify that the number of irrigation needed does not exceed the number of
+                        // irrigation available
+                        if (botState.getInventory().getIrrigationChannelsCount()
+                                > possibleEdgePositions.size()) {
+                            allPossiblePaths.add(possibleEdgePositions);
+                        }
                     }
                 }
 
@@ -181,6 +221,7 @@ public class GeneralTacticBot implements Bot {
                 allPossiblePaths.stream()
                         .min(Comparator.comparingInt(List::size))
                         .ifPresent(irrigationToPlace::addAll);
+                console.displayDebug("BIG BRAIN MODE - irrigationToPlace: " + irrigationToPlace);
             }
         }
     }
@@ -200,20 +241,22 @@ public class GeneralTacticBot implements Bot {
     /**
      * Get all the shape matching the patterns on the current pattern objectives
      *
-     * @param board the board
+     * @param board    the board
      * @param botState the bot state
      * @return the list of shapes matching the pattern objectives
      */
-    public List<Shape> getCandidateShape(Board board, BotState botState) {
+    public List<List<Shape>> getCandidateShapes(Board board, BotState botState) {
         List<PatternObjective> patternObjectives =
                 botState.getObjectives().stream()
                         .filter(PatternObjective.class::isInstance)
                         .map(PatternObjective.class::cast)
                         .toList();
 
-        List<Shape> matchedPatterns = new ArrayList<>();
+        console.displayDebug("BIG BRAIN MODE - patternObjectives: " + patternObjectives);
+
+        List<List<Shape>> matchedPatterns = new ArrayList<>();
         for (PatternObjective patternObjective : patternObjectives) {
-            matchedPatterns = patternObjective.getPattern().match(board, true);
+            matchedPatterns.add(patternObjective.getPattern().match(board, true));
         }
 
         return matchedPatterns;
