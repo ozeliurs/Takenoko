53d46a58135809ee989e38c6c60686a332a3503e
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-02-07 17:44:55 +0100

    fix: rename method to correct one and add todo
    Refs: #391

--- src/test/java/com/takenoko/objective/PatternObjectiveTest.java
+++ src/test/java/com/takenoko/objective/PatternObjectiveTest.java
@@ -155,9 +155,9 @@ class PatternObjectiveTest {
             Pattern pattern = mock(Pattern.class);
             PatternObjective patternObjective = new PatternObjective(pattern, 0);
             patternObjective.getCompletion(mock(Board.class), mock(BotState.class));
-            verify(pattern).getSubsetMatchPattern(any());
+            verify(pattern).matchRatio(any());
             // then verify that it returns the same value as the pattern
-            when(pattern.getSubsetMatchPattern(any())).thenReturn(1f);
+            when(pattern.matchRatio(any())).thenReturn(1f);
             assertThat(patternObjective.getCompletion(mock(Board.class), mock(BotState.class)))
                     .isEqualTo(1f);
         }
