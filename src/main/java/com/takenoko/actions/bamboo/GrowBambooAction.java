7c48b8abce73a922c3fc4cdd235ad9197b2c2e7e
Maxime BILLY <maxime.billy@etu.unice.fr>
2023-02-09 12:42:05 +0100

    refactor: refactor var @Apoorva64
    @Apoorva64 Ref. #427
    
    @Apoorva64

--- src/main/java/com/takenoko/actions/bamboo/GrowBambooAction.java
+++ src/main/java/com/takenoko/actions/bamboo/GrowBambooAction.java
@@ -22,12 +22,13 @@ public class GrowBambooAction implements Action {
     @Override
     public ActionResult execute(Board board, BotManager botManager) {
         if (board.isBambooGrowableAt(position)) {
-            LayerBambooStack l = board.growBamboo(position);
+            LayerBambooStack layerBambooStack = board.growBamboo(position);
             botManager.displayMessage("Grew bamboo at " + position);
             botManager
                     .getSingleBotStatistics()
                     .updatePlantedBambooCounter(
-                            board.getTileAt(position).getColor(), l.getBambooCount());
+                            board.getTileAt(position).getColor(),
+                            layerBambooStack.getBambooCount());
             return new ActionResult();
         }
         throw new IllegalStateException("Cannot grow bamboo at " + position);
