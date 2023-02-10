2f111375f0d2dab57c9d2bce8dff7e1ff295ef07
Raphaël Anjou <raphael.anjou@etu.univ-cotedazur.fr>
2023-02-10 09:44:12 +0100

    chore: remove useless objective types
    #Refs: #412

--- src/main/java/com/takenoko/objective/ObjectiveTypes.java
+++ src/main/java/com/takenoko/objective/ObjectiveTypes.java
@@ -1,12 +1,7 @@
 package com.takenoko.objective;
 
 public enum ObjectiveTypes {
-    NUMBER_OF_TILES_PLACED,
-    TWO_ADJACENT_TILES,
-    MOVED_PANDA,
-    BAMBOO_STACK,
     SHAPE,
-    NUMBER_OF_BAMBOOS_EATEN,
     GARDENER,
     PANDA,
     EMPEROR,
