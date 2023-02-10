8f7e03d561af8606c4cbac1b485d5a37bff635a0
Sayf <sayf-eddine.halmi@etu.unice.fr>
2023-02-10 19:37:02 +0100

    feat: add methods equals and hashcode to GameProgressStatistics Ref: #457

--- src/main/java/com/takenoko/stats/GameProgressStatistics.java
+++ src/main/java/com/takenoko/stats/GameProgressStatistics.java
@@ -14,6 +14,7 @@ import com.takenoko.objective.Objective;
 import com.takenoko.objective.ObjectiveType;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
 
 public class GameProgressStatistics {
     private final GameProgress gameProgress;
@@ -127,5 +128,18 @@ public class GameProgressStatistics {
             updateRedeemedObjectives(latestHistoryItems.redeemedObjectives());
             updateActions(latestHistoryItems.action());
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GameProgressStatistics that = (GameProgressStatistics) o;
+        return totalNbOfRounds == that.totalNbOfRounds && totalNbOfAction == that.totalNbOfAction && totalNbOfRedeemedObjectives == that.totalNbOfRedeemedObjectives && gameProgress == that.gameProgress && actions.equals(that.actions) && objectives.equals(that.objectives);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(gameProgress, totalNbOfRounds, totalNbOfAction, actions, totalNbOfRedeemedObjectives, objectives);
     }
 }
