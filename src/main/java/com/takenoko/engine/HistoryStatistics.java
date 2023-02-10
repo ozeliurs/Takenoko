8835c94534c0b0e1c09e835ad4dcdd0d3f0a849a
Sayf <sayf-eddine.halmi@etu.unice.fr>
2023-02-10 17:56:55 +0100

    feat: improve HistoryStatistics Ref: #433

--- src/main/java/com/takenoko/engine/HistoryStatistics.java
+++ src/main/java/com/takenoko/engine/HistoryStatistics.java
@@ -1,26 +1,40 @@
 package com.takenoko.engine;
 
+import com.takenoko.bot.utils.GameProgress;
 import java.util.HashMap;
 import java.util.List;
 
-public class HistoryStatistics extends HashMap<BotManager,HistoryStatisticsItem> {
+public class HistoryStatistics extends HashMap<BotManager, HistoryStatisticsItem> {
 
-    public HistoryStatistics(List<BotManager> botManagers){
-        for(BotManager botManager:botManagers){
-            put(botManager,botManager.getHistoryStatisticsItem());
+    public HistoryStatistics() {
+        super();
+    }
+
+    public HistoryStatistics(List<BotManager> botManagers) {
+        for (BotManager botManager : botManagers) {
+            put(botManager, new HistoryStatisticsItem());
         }
     }
 
     @Override
-    public String toString(){
-        StringBuilder historyStatistics=new StringBuilder();
-        for(Entry<BotManager, HistoryStatisticsItem> entry:entrySet()){
-            historyStatistics.append("======")
+    public String toString() {
+        StringBuilder historyStatistics = new StringBuilder();
+        for (Entry<BotManager, HistoryStatisticsItem> entry : entrySet()) {
+            historyStatistics
+                    .append("======")
                     .append(entry.getKey())
                     .append("======\n")
                     .append(entry.getValue())
                     .append("\n");
         }
         return historyStatistics.toString();
+    }
+
+    public void updateEvolution(BotManager botManager, History history) {
+        get(botManager).updateEvolution(history, botManager.getUniqueID());
+    }
+
+    public void incrementNumberOfRounds(BotManager botManager, GameProgress gameProgress) {
+        get(botManager).get(gameProgress).incrementNbOfRounds();
     }
 }
